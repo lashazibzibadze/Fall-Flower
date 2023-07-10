66f8d06f50c870b2e7f726c27c3b1ded50a864e6
Lasha Zibzibadze <101782753+lashazibzibadze@users.noreply.github.com>
2023-07-10 13:38:12 +0400

    Add files via upload

---
+++ FallFlower.py
@@ -0,0 +1,20 @@
+#Name:  Lasha Zibzibadze
+#Date: February, 2022
+#This program draws a fall flower.
+
+import turtle
+
+thomasH = turtle.Turtle()
+thomasH.color("hotpink")
+
+for i in range(36):
+    thomasH.forward(100)
+    thomasH.left(145)
+    thomasH.forward(10)
+    thomasH.right(90)
+    thomasH.forward(10)
+    thomasH.left(135)
+    thomasH.forward(100)
+
+
+wn.exitonlick()    
